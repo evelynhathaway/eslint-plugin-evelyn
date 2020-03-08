f13797a2f44eca12f01b956fc3de9ff551663672
Evelyn Hathaway <git@evelyn.dev>
2020-03-08 02:08:26 -0700

    Chore: Add basic default config test

---
+++ test/configs/default.js
@@ -0,0 +1,21 @@
+const config = require("../../lib/configs/default.js");
+const configTester = require("eslint-config-rule-tester");
+
+configTester("config: default", config, {
+	valid: [
+		"function test () {\n	test();\n}\ntest();\n(() => {})();",
+	],
+	invalid: [
+		// Indentation
+		{
+			code: "function test () {\n    test();\n}\ntest();",
+			errors: ["Expected indentation of 1 tab but found 4 spaces."],
+		},
+		// Semicolons protecting unexpected errors
+		// Example parsing error: `const test = {test: "test"}\n(() => {})();`
+		{
+			code: "const test = {test: \"test\"}\nvoid(test)",
+			errors: ["Missing semicolon.", "Missing semicolon."],
+		},
+	],
+});
