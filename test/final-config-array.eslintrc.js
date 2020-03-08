a0323ac388cb70a475549ac65d83f4475a9213a4
Evelyn Hathaway <git@evelyn.dev>
2020-03-08 02:12:21 -0700

    Chore: Test the final config for errors, export the final config for tacking

---
+++ test/final-config-array.eslintrc.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line node/no-extraneous-require
+const {stealFinalConfigArray} = require("eslint-plugin-evelyn/lib/save-config");
+const chalk = require("chalk");
+
+const alertAllErrors = function (configs) {
+	let hadError = false;
+	const alertIfError = object => {
+		const error = object && object.error;
+		if (!error) return;
+
+		// eslint-disable-next-line no-console
+		console.error(`\n${chalk.red(error.code)}: ${error.message}}`);
+		hadError = true;
+	};
+
+	// Get all errors
+	for (const config of configs) {
+		alertIfError(config.parser);
+		for (const key in config.plugins) {
+			alertIfError(config.plugins[key]);
+		}
+	}
+
+	// eslint-disable-next-line no-process-exit, unicorn/no-process-exit
+	if (hadError) process.exit(1);
+};
+stealFinalConfigArray(
+	alertAllErrors
+);
+
+
+module.exports = require("../all-configs.eslintrc.js");
